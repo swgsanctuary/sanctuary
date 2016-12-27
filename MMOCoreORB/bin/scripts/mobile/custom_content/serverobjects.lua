d62326809a6c9d9bd713ab3986a3ba3bd8979ec9
lasko2112 <coll56667@yahoo.co.uk>
2016-12-27 09:04:37 +0000

    [Added] More CU/NGE Mobs

--- MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
@@ -2,6 +2,7 @@
 
 --Child Folders
 includeFile("custom_content/ep3/serverobjects.lua")
+includeFile("custom_content/mobile/serverobjects.lua")
 includeFile("custom_content/som/serverobjects.lua")
 
 
