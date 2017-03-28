dfd306d57f99127021bc43e2baeebf89f055f812
lasko2112 <coll56667@yahoo.co.uk>
2017-03-28 11:45:47 +0100

    [Added] Endor object

---
+++ MMOCoreORB/bin/scripts/object/custom_content/building/endor/objects.lua
@@ -0,0 +1,4 @@
+object_building_endor_shared_endor_catwalk_cap = SharedStaticObjectTemplate:new {
+	clientTemplateFileName = "object/building/endor/shared_endor_catwalk_cap.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_endor_shared_endor_catwalk_cap, "object/building/endor/shared_endor_catwalk_cap.iff")
---
+++ MMOCoreORB/bin/scripts/object/custom_content/building/endor/serverobjects.lua
@@ -0,0 +1,1 @@
+includeFile("custom_content/building/endor/endor_catwalk_cap.lua")
