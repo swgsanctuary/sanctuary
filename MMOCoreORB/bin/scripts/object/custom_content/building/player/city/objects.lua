37b521cc7ac620b1cdd3f14deef20384072cd622
lasko2112 <coll56667@yahoo.co.uk>
2017-03-28 09:20:32 +0100

    [Added] Player city objects

---
+++ MMOCoreORB/bin/scripts/object/custom_content/building/player/city/objects.lua
@@ -0,0 +1,53 @@
+object_building_player_city_shared_barn_no_planet_restriction = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_barn_no_planet_restriction.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_barn_no_planet_restriction, "object/building/player/city/shared_barn_no_planet_restriction.iff")
+
+--*************************************
+
+object_building_player_city_shared_city_imperial_fountain = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_city_imperial_fountain.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_city_imperial_fountain, "object/building/player/city/shared_city_imperial_fountain.iff")
+
+--*************************************
+
+object_building_player_city_shared_city_imperial_statue = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_city_imperial_statue.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_city_imperial_statue, "object/building/player/city/shared_city_imperial_statue.iff")
+
+--*************************************
+
+object_building_player_city_shared_city_rebel_fountain = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_city_rebel_fountain.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_city_rebel_fountain, "object/building/player/city/shared_city_rebel_fountain.iff")
+
+--*************************************
+
+object_building_player_city_shared_city_rebel_statue = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_city_rebel_statue.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_city_rebel_statue, "object/building/player/city/shared_city_rebel_statue.iff")
+
+--*************************************
+
+object_building_player_city_shared_diner_no_planet_restriction = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_diner_no_planet_restriction.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_diner_no_planet_restriction, "object/building/player/city/shared_diner_no_planet_restriction.iff")
+
+--*************************************
+
+object_building_player_city_shared_garden_imperial_01 = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_garden_imperial_01.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_garden_imperial_01, "object/building/player/city/shared_garden_imperial_01.iff")
+
+--*************************************
+
+object_building_player_city_shared_garden_rebel_01 = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/building/player/city/shared_garden_rebel_01.iff"
+}
+ObjectTemplates:addClientTemplate(object_building_player_city_shared_garden_rebel_01, "object/building/player/city/shared_garden_rebel_01.iff")
